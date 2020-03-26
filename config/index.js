e4105c98f26b52260fe1e3e96bf970074dd2b866
HQJ <719676340@qq.com>
2020-03-26 23:22:04 +0800

    更改了本地服务器地址3000

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': { // 匹配所有以 '/api'开头的请求路径
-      target: 'http://localhost:4001/users', // 代理目标的基础路径
+      // target: 'http://localhost:4001/users', // 代理目标的基础路径
+      target: 'http://localhost:3000/users', // 代理目标的基础路径
       changeOrigin: true, // 支持跨域
       pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
         '^/api': ''
